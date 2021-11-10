73bdf93b503b60df42446b78f4e53917fa72f594
GilmarMMJr <gilmar_mendonca@live.com>
2021-11-10 11:38:15 -0300

    Configuração inicial do projeto

---
+++ Clima/Clima/Clima.h
@@ -0,0 +1,18 @@
+//
+//  Clima.h
+//  Clima
+//
+//  Created by Gilmar Junior on 10/11/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Clima.
+FOUNDATION_EXPORT double ClimaVersionNumber;
+
+//! Project version string for Clima.
+FOUNDATION_EXPORT const unsigned char ClimaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Clima/PublicHeader.h>
+
+
