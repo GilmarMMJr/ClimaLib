62e5c0404b3a69142e93a8c7c481b146bee71f3c
GilmarMMJr <gilmar_mendonca@live.com>
2021-11-10 15:01:48 -0300

    Adicionando casos de testes

---
+++ Clima/Clima/ClimaLib.swift
@@ -0,0 +1,22 @@
+//
+//  Climalib.swift
+//  Clima
+//
+//  Created by Gilmar Junior on 10/11/21.
+//
+
+import Foundation
+
+
+public final class ClimaLib{
+    
+    let name = "ClimaLib"
+    
+    public func add(a: Int, b: Int) -> Int{
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+}
